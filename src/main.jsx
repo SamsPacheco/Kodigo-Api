dc47380278701669063fd4155aad4dadcd0a38b9
Samuel Pacheco <142636362+SamsPacheco@users.noreply.github.com>
2025-07-04 10:50:29 -0600

    configuracion de bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import { KodigoApiApp } from './KodigoApiApp.jsx'
 import { BrowserRouter } from 'react-router'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+import 'bootstrap/dist/css/bootstrap.min.css'; // <-- Bootstrap CSS
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // <-- JS Bundle (con Popper)
+// import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <BrowserRouter>
-      <KodigoApiApp />
+        <KodigoApiApp />
     </BrowserRouter>
-  </StrictMode>,
-)
+  </React.StrictMode>,
+);
--- src/router/RouterApp.jsx
+++ src/router/RouterApp.jsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { Navigate, Route, Routes } from 'react-router'
 import { Accomodations, Bookings, Dashboard, Login } from '../pages'
 
