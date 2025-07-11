bf82fdfa781b1f0b7700ea7dcfd0178998307e0e
Samuel Pacheco <142636362+SamsPacheco@users.noreply.github.com>
2025-07-11 12:59:33 -0600

    uso de la api
    alojamientos y rutas

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,27 +1,36 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import { AccommodationCard, Sidebar, ModalAlojamiento } from '../components';
+import { getAccomodation } from '../services/accomodationsService';
 
 export const Dashboard = () => {
+
+  const [accommodations, setAccommodations] = useState([]);
+
+  useEffect(() => {
+      getData();
+  }, [])
+  
+
+  const getData = async() => {
+    const data = await getAccomodation();
+    setAccommodations(data)
+  }
 
   const [mostrarModalAlojamientos, setMostrarModalAlojamientos] = useState(false);
 
   return (
     <section className="d-flex" style={{height: '100dvh'}}>
       <Sidebar />
-      <main className="p-4 flex-grow-1 position-relative">
-        <div className="d-flex justify-content-between align-items-center mb-4">
-          <h3>Alojamientos</h3>
-          <button className="btn btn-dark d-none d-md-block" onClick={() => {setMostrarModalAlojamientos(true)}} >
+      <main className="py-3 px-md-4 flex-grow-1 position-relative overflow-y-auto">
+        <section className="d-flex justify-content-between align-items-center mb-3 px-1 px-md-3">
+          <h3 className='fw-semibol'>Alojamientos</h3>
+          <button className="btn btn-dark mx-2 mx-md-0" onClick={() => {setMostrarModalAlojamientos(true)}} >
             <i className="bi bi-plus me-1"></i> Nuevo Alojamiento
           </button>
-
-          <button className="btn btn-dark d-flex align-items-center rounded-circle position-absolute bottom-0 end-0 m-4 d-md-none">
-            <i className="bi bi-plus fs-2 fw-bolder"></i>
-          </button>
           
-        </div>
+        </section>
 
-        <AccommodationCard />
+        <AccommodationCard data = {accommodations}/>
       </main>
 
       {mostrarModalAlojamientos && (
